8bbdfa1adc51f1b95d47e3b2a37a201a8da09785
gauravyadv <gauravyadavry_cse18@its.edu.in>
2023-12-22 20:31:51 +0530

    .

---
+++ server/controllers/bookController.js
@@ -0,0 +1,93 @@
+
+const Book = require('../models/bookModel');
+
+async function getAllBooks(req, res) {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+async function addBook(req, res) {
+  const { id, title, author, isbn, published_at, copies } = req.body;
+
+  try {
+    const newBook = new Book({ id, title, author, isbn, published_at, copies });
+    await newBook.save();
+
+    res.status(201).json({ message: 'Book added successfully', book: newBook });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+//const { ObjectID } = require('mongodb');  // Import ObjectID from MongoDB
+
+const { ObjectID } = require('mongodb');
+
+
+const { ObjectId } = require('mongodb');
+
+async function updateBook(req, res) {
+  const { id } = req.params;
+  const { title, author, isbn, published_at, copies } = req.body;
+
+  try {
+    // Convert id to ObjectId type
+    const objectId = new ObjectId(id);
+
+    const updatedBook = await Book.findOneAndUpdate(
+      { _id: objectId }, // Use _id instead of id, assuming _id is the default MongoDB ObjectId
+      { title, author, isbn, published_at, copies },
+      { new: true }
+    );
+
+    console.log('hlw', updatedBook);
+
+    if (!updatedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    res.status(200).json({ message: 'Book updated successfully', book: updatedBook });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+
+  
+
+
+
+async function deleteBook(req, res) {
+  const { id } = req.params;
+
+  try {
+    // Convert id to ObjectId type
+    const objectId = new ObjectId(id);
+
+    const deletedBook = await Book.findOneAndDelete({ _id: objectId });
+
+    if (!deletedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    res.status(200).json({ message: 'Book deleted successfully', book: deletedBook });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+
+module.exports = {
+  getAllBooks,
+  addBook,
+  updateBook,
+  deleteBook,
+};
---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+// models/userModel.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  checkedOutBooks: [
+    {
+      bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
+      checkoutDate: { type: Date, default: Date.now },
+    },
+  ],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
