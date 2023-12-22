da977fc29c09231711f821a1d663b0cdf96006ed
gauravyadv <gauravyadavry_cse18@its.edu.in>
2023-12-22 17:38:19 +0530

    commit

--- server/app/app.js
+++ server/app/app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const authRoutes = require('../routes/authRoutes');
-const bookRoutes = require('../routes/bookRoutes');
-const checkoutRoutes = require('../routes/checkoutRoutes');
-const mongoose = require('mongoose')
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("../routes/authRoutes");
+const bookRoutes = require("../routes/bookRoutes");
+const checkoutRoutes = require("../routes/checkoutRoutes");
+const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 
 app.use(express.json());
+// Use CORS middleware
+app.use(cors());
 
 // Connect to MongoDB
 
-
-const dbURI = 'mongodb+srv://gauravyadava963998:LzVqwT0nQQ37CMOt@cluster0.yj6gutd.mongodb.net/MachineTest?retryWrites=true&w=majority';
+const dbURI =
+  "mongodb+srv://gaurav:Gaurav123@cluster0.llvrfzy.mongodb.net/";
 
 try {
   mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,36 +23,35 @@ try {
   const dbConnection = mongoose.connection;
 
   // Event listener for successful connection
-  dbConnection.on('connected', () => {
-    console.log('MongoDB connected successfully');
+  dbConnection.on("connected", () => {
+    console.log("MongoDB connected successfully");
   });
 
   // Event listener for connection error
-  dbConnection.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
+  dbConnection.on("error", (err) => {
+    console.error("MongoDB connection error:", err);
   });
 
   // Event listener for disconnection
-  dbConnection.on('disconnected', () => {
-    console.log('MongoDB disconnected');
+  dbConnection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
   });
 
   // Close the Mongoose connection when the Node process terminates
-  process.on('SIGINT', () => {
+  process.on("SIGINT", () => {
     dbConnection.close(() => {
-      console.log('MongoDB connection closed through app termination');
+      console.log("MongoDB connection closed through app termination");
       process.exit(0);
     });
   });
 } catch (error) {
-  console.error('Error connecting to MongoDB:', error.message);
+  console.error("Error connecting to MongoDB:", error.message);
 }
 
-
 // Routes
-app.use('/auth', authRoutes);
-app.use('/books', bookRoutes);
-app.use('/checkouts', checkoutRoutes);
+app.use("/auth", authRoutes);
+app.use("/books", bookRoutes);
+app.use("/checkouts", checkoutRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
